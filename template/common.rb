be40bb11f95c4e219427573fa2612d16596cfad2
Ziphil <yfe23457@nifty.com>
2020-02-23 22:47:19 +0900

    変更

--- template/common.rb
+++ template/common.rb
@@ -3,6 +3,30 @@
 
 COLORS = {1 => "blue", 2 => "orange", 3 => "pink", 4 => "green"}
 
+converter.add(["slide"], ["root"]) do |element, _, number|
+  this = ""
+  slide_number = element.attribute("number").to_s.to_i
+  Tag.repeat(element) do |count|
+    variables[:slide_count] = variables[:slide_count].to_i + 1
+    this << Tag.build("div", "slide") do |this|
+      this << Tag.build("div", "number") do |this|
+        this << Tag.build("span", "section") do |this|
+          this << number.to_s
+        end
+        this << Tag.build("span", "subsection") do |this|
+          this << slide_number.to_s
+        end
+      end
+      this << Tag.new("div", "header")
+      this << Tag.new("div", "footer")
+      this << Tag.build("div", "content") do |this|
+        this << apply(element, "root", count)
+      end
+    end
+  end
+  next this
+end
+
 converter.add(["xl"], ["root"]) do |element, _, count|
   this = ""
   this << Tag.build("ul", "conlang") do |this|
--- template/main.rb
+++ template/main.rb
@@ -11,30 +11,6 @@ converter.add(["root"], [""]) do |element|
   next result
 end
 
-converter.add(["slide"], ["root"]) do |element, _, number|
-  this = ""
-  slide_number = element.attribute("number").to_s.to_i
-  Tag.repeat(element) do |count|
-    variables[:slide_count] = variables[:slide_count].to_i + 1
-    this << Tag.build("div", "slide") do |this|
-      this << Tag.build("div", "number") do |this|
-        this << Tag.build("span", "section") do |this|
-          this << number.to_s
-        end
-        this << Tag.build("span", "subsection") do |this|
-          this << slide_number.to_s
-        end
-      end
-      this << Tag.new("div", "header")
-      this << Tag.new("div", "footer")
-      this << Tag.build("div", "content") do |this|
-        this << apply(element, "root", count)
-      end
-    end
-  end
-  next this
-end
-
 converter.add(["x", "xn"], [//]) do |element, scope, *args|
   this = ""
   this << Tag.build("span", "sans") do |this|
